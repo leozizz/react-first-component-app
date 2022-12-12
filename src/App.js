f715b08558cbc1a2cded43fcb4526c9614ca629d
leozizz <leonardocps2015@gmail.com>
2022-12-12 16:19:40 -0300

    Inserindo componentes Header e Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './App.css';
 
+import Header from './componentes/Header/'
+import Footer from './componentes/Footer/'
+
 function App() {
   return (
     <div className='App'>
-      
+      <Header />
+      <Footer />
     </div>
   );
 }
