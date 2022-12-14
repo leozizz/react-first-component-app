07b4111e284486ab40ec8c6e1d1306fd0512371c
leozizz <leonardocps2015@gmail.com>
2022-12-14 15:57:47 -0300

    Estilizando componentes

--- src/componentes/Card/index.js
+++ src/componentes/Card/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './style.css'
 
 export default function Card() {
     return (
-        <div>
-            <p>Componentes</p>
+        <div className='card-container'>
+            <p>Componentes:</p>
             <p>Facilita em manter partes menores funcionando corretamente</p>
             <p>Você pode reutilizá-los, ou seja, menos código para se escrever</p>
         </div>
